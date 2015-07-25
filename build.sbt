cacd49af956589d41f1ffc24afa6ae6d47db5ed5
Ernest Romero <ernest@unstable.build>
2015-07-24 22:19:58 -0700

    Improved compatibility with hive + cosmetic changes 😎

--- build.sbt
+++ build.sbt
@@ -6,9 +6,9 @@ version := "0.2"
 
 scalaVersion := "2.11.6"
 
-javacOptions := Seq("-target", "1.7", "-source", "1.7")
+javacOptions := Seq("-target", "1.6", "-source", "1.6", "-Xlint:-options")
 
-scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8", "-target:jvm-1.7")
+scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8", "-target:jvm-1.6")
 
 resolvers += "Cascading repo" at "http://conjars.org/repo"
 
@@ -21,7 +21,7 @@ resolvers ++= Seq(
 libraryDependencies ++= Seq(
   "org.apache.hadoop" % "hadoop-core" % "1.1.1" % "provided",
   "org.apache.hive" % "hive-exec" % "0.9.0" % "provided",
-  "com.google.protobuf" % "protobuf-java" % "2.6.1",
+  "com.google.protobuf" % "protobuf-java" % "2.4.1",
   "joda-time" % "joda-time" % "2.8.1",
   "org.joda" % "joda-convert" % "1.7",
   "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
--- src/main/scala/com/opentok/udfs/DecodeVed.scala
+++ src/main/scala/com/opentok/udfs/DecodeVed.scala
@@ -1,7 +1,5 @@
 package com.opentok.udfs
 
-import java.io.FileOutputStream
-
 import com.opentok.udfs.Ved.VedMessage
 import com.typesafe.scalalogging.Logger
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF
@@ -9,55 +7,77 @@ import org.apache.hadoop.hive.ql.udf.generic.GenericUDF.DeferredObject
 import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector.PrimitiveCategory
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory
 import org.apache.hadoop.hive.serde2.objectinspector.{ObjectInspector, ObjectInspectorFactory, PrimitiveObjectInspector, StructObjectInspector}
-import org.joda.time.{DateTimeZone, DateTime}
+import org.joda.time.{DateTime, DateTimeZone}
 import org.slf4j.LoggerFactory
 
 import scala.annotation.tailrec
 import scala.collection.JavaConversions._
-import scala.util.{Failure, Try}
+import scala.util.Try
 
 /**
  * Inspired by: https://moz.com/blog/inside-googles-ved-parameter
- * Compile protocol buffer first: protoc --java_out=src/main/java src/main/resources/ved.proto
+ *
+ * Compile .proto file with protoc compiler v2.4.1:
+ * protoc --java_out=src/main/java src/main/resources/ved.proto
  */
 class DecodeVed extends GenericUDF {
 
+  val log = Logger(LoggerFactory.getLogger("VedDecoder"))
+
   object Decoder {
 
-    val log = Logger(LoggerFactory.getLogger("VedDecoder"))
+    /**
+     * Attempts to interpret and translate ved messages.
+     * Note that the heavy use of null is due to compatibility with hive
+     */
+    implicit class PimpedVedMessage(v: Ved.VedMessage) {
 
-    val stringOI = PrimitiveObjectInspectorFactory.getPrimitiveJavaObjectInspector(PrimitiveCategory.STRING)
-    val longOI = PrimitiveObjectInspectorFactory.getPrimitiveJavaObjectInspector(PrimitiveCategory.LONG)
-    val boolOI = PrimitiveObjectInspectorFactory.getPrimitiveJavaObjectInspector(PrimitiveCategory.BOOLEAN)
-
-    val TYPES = Map[String, ObjectInspector]("link_index" → stringOI,
-      "link_type" → stringOI, "sub_result_position" → longOI,
-      "result_position" → longOI, "page" → stringOI, "timestamp" → stringOI,
-      "dt" → stringOI, "mysterious1" → longOI, "mysterious2" → longOI
-    )
-
-    implicit class Translator(n: Long) {
-
-      def parseIndex: String = Try(n match {
-        case a if a > 85 ⇒ "right_hand_column_or_bottom"
-        case a if a > 45 ⇒ "main_column"
-        case _ ⇒ null
-      }).getOrElse(null)
-
-      def parsePage(v: Ved.VedMessage): String = Try {
-        if (!v.hasStartResultPosition && v.getLinkType == Ved.VedMessage.Type.sponsored_search_result) "page_1"
-        else if (v.hasStartResultPosition) "page_" + (n / 10 + 1)
-        else null
-      }.getOrElse(null)
-
-      def toDateTime(pattern: String): String = Try {
+      private def toDateTime(n: Long, pattern: String): String = Try {
         log.info(s"Converting microseconds since epoch ($n) into timestamp")
         new DateTime(n / 1000, DateTimeZone.UTC)
           .toDateTime(DateTimeZone.forID("America/Los_Angeles"))
           .toString(pattern)
       }.getOrElse(null)
 
-      def toNonZeroBasedNum: Long = n + 1
+      def linkIndex: String = if (v.hasLinkIndex) v.getLinkIndex match {
+        case a if a > 85 ⇒ "right_hand_column_or_bottom"
+        case a if a > 45 ⇒ "main_column"
+        case _ ⇒ null
+      } else null
+
+      def linkType: String = if (v.hasLinkType) v.getLinkType.toString else null
+
+      def subResultPosition: String =
+        if (v.hasSubResultPosition) (v.getSubResultPosition + 1).toString else null
+
+      def resultPosition: String =
+        if (v.hasResultPosition) (v.getResultPosition + 1).toString else null
+
+      def page: String = Try(v.getLinkType match {
+        case Ved.VedMessage.Type.sponsored_search_result ⇒ "page_1"
+        case _ if v.hasStartResultPosition ⇒ "page_" + (v.getStartResultPosition / 10 + 1)
+        case _ ⇒ null
+      }).getOrElse(null)
+
+      def timestamp: String =
+        if (v.getMysteriousMsg.getNestedMysteriousMsg.hasTs)
+          toDateTime(v.getMysteriousMsg.getNestedMysteriousMsg.getTs, "yyyy-MM-dd HH:mm:ss.SSS")
+        else null
+
+      def dt: String =
+        if (v.getMysteriousMsg.getNestedMysteriousMsg.hasTs)
+          toDateTime(v.getMysteriousMsg.getNestedMysteriousMsg.getTs, "yyyy-MM-dd")
+        else null
+
+      def mysterious1: String =
+        if (v.getMysteriousMsg.getNestedMysteriousMsg.hasMysterious1)
+          v.getMysteriousMsg.getNestedMysteriousMsg.getMysterious1.toString
+        else null
+
+      def mysterious2: String =
+        if (v.getMysteriousMsg.getNestedMysteriousMsg.hasMysterious2)
+          v.getMysteriousMsg.getNestedMysteriousMsg.getMysterious2.toString
+        else null
 
     }
 
@@ -90,17 +110,8 @@ class DecodeVed extends GenericUDF {
     }
 
     def toArray(v: VedMessage): Try[Array[Any]] = Try {
-      Array(
-        if (v.hasLinkIndex) v.getLinkIndex.parseIndex else null, //getField returns 0 or base enum if hasField is false. In hive we prefer NULL when data is not available
-        if (v.hasLinkType) v.getLinkType.toString else null,
-        if (v.hasSubResultPosition) v.getSubResultPosition.toNonZeroBasedNum else null,
-        if (v.hasResultPosition) v.getResultPosition.toNonZeroBasedNum else null,
-        v.getStartResultPosition.parsePage(v),
-        if (v.getUnknownMsg.getNestedUnknownMsg.hasTs) v.getUnknownMsg.getNestedUnknownMsg.getTs.toDateTime("yyyy-MM-dd HH:mm:ss.SSS") else null,
-        if (v.getUnknownMsg.getNestedUnknownMsg.hasTs) v.getUnknownMsg.getNestedUnknownMsg.getTs.toDateTime("yyyy-MM-dd") else null,
-        if (v.getUnknownMsg.getNestedUnknownMsg.hasUnknown1) v.getUnknownMsg.getNestedUnknownMsg.getUnknown1 else null,
-        if (v.getUnknownMsg.getNestedUnknownMsg.hasUnknown2) v.getUnknownMsg.getNestedUnknownMsg.getUnknown2 else null
-      )
+      Array(v.linkIndex, v.linkType, v.subResultPosition, v.resultPosition,
+        v.page, v.timestamp, v.dt, v.mysterious1, v.mysterious2)
     }
 
     @tailrec
@@ -113,7 +124,7 @@ class DecodeVed extends GenericUDF {
             s + "=" * (4 - s.length % 4) //add padding
           })).flatMap(toArray)
         case Some(_) ⇒ decode(msg.tail)
-        case None ⇒ Try(new Array[Any](TYPES.toList.length))
+        case None ⇒ Try(new Array[Any](FIELDS.length))
       }
     }
 
@@ -121,17 +132,19 @@ class DecodeVed extends GenericUDF {
       decode(msg).recover {
         case e: Throwable ⇒
           log.error(s"There was an error when decoding ved code: $e")
-          new Array[Any](TYPES.toList.length)
+          new Array[Any](FIELDS.length)
       }.get
   }
 
   private var inputInspector: PrimitiveObjectInspector = _
 
+  val stringOI = PrimitiveObjectInspectorFactory.getPrimitiveJavaObjectInspector(PrimitiveCategory.STRING)
+  val FIELDS = List("link_index", "link_type", "sub_result_position", "result_position", "page", "timestamp", "dt", "mysterious1", "mysterious2")
+
   def initialize(inputs: Array[ObjectInspector]): StructObjectInspector = {
     this.inputInspector = inputs(0).asInstanceOf[PrimitiveObjectInspector]
-    val outputFieldNames = Decoder.TYPES.keys.toList
-    val outputInspectors = Decoder.TYPES.values.toList
-    ObjectInspectorFactory.getStandardStructObjectInspector(outputFieldNames, outputInspectors)
+    val outputInspectors = List(stringOI, stringOI, stringOI, stringOI, stringOI, stringOI, stringOI, stringOI, stringOI)
+    ObjectInspectorFactory.getStandardStructObjectInspector(FIELDS, outputInspectors)
   }
 
   def getDisplayString(children: Array[String]): String = {
